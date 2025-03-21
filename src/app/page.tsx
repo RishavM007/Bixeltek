b7b7b2eb357dc0a5954b9ef83ece0c65bef8a85e
RishavM007 <solarisxavier@gmail.com>
2025-03-21 20:36:33 +0530

    home fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+export const metadata = {
+  title: "Bixeltek | Best Digital Marketing Company in Hyderabad",
+  description:
+    "Bixeltek is the leading digital marketing company in Hyderabad. We specialize in Google Ads, SEO, social media marketing, and performance-driven campaigns to boost your business growth.",
+  keywords: [
+    "digital marketing company Hyderabad",
+    "best digital marketing agency Hyderabad",
+    "Google Ads agency Hyderabad",
+    "SEO services Hyderabad",
+    "social media marketing Hyderabad",
+    "PPC agency Hyderabad",
+  ],
+  openGraph: {
+    title: "Bixeltek | Best Digital Marketing Company in Hyderabad",
+    description:
+      "Looking to grow your business? Bixeltek offers expert digital marketing services in Hyderabad, including SEO, Google Ads, and social media marketing. Get in touch today!",
+    siteName: "Bixeltek",
+    type: "website",
+  },
+  
+};
+
+export const dynamic = "force-dynamic"; // Ensures SSR (must be below metadata)
+
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { Screen } from "@/sections/Screen";
@@ -9,20 +33,21 @@ import { Casestudy } from "@/sections/Casestudies";
 import { About } from "@/sections/About";
 import { Footer } from "@/sections/Footer";
 import { ContactButton } from "@/sections/contactButton";
+
 export default function Home() {
   return (
     <>
-    <Header />
-    <Hero />
-    <Screen />
-    <LogoTicker />
-    <About />
-    <Features />
-    <Testimonials />
-    <CallToAction />
-    <Casestudy />
-    <ContactButton />
-    <Footer />
+      <Header />
+      <Hero />
+      <Screen />
+      <LogoTicker />
+      <About />
+      <Features />
+      <Testimonials />
+      <CallToAction />
+      <Casestudy />
+      <ContactButton />
+      <Footer />
     </>
   );
 }
