08969847e7f2c16ea17c2aabdffc8cb1a51bcb69
RishavM007 <solarisxavier@gmail.com>
2025-04-04 17:41:43 +0530

    test

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -1,47 +1,47 @@
-"use client";
+// "use client";
 
-import { useEffect, useState } from "react";
-import { grid } from "ldrs";
+// import { useEffect, useState } from "react";
+// import { grid } from "ldrs";
 
-if (typeof window !== "undefined") {
-  grid.register();
-}
+// if (typeof window !== "undefined") {
+//   grid.register();
+// }
 
-export const Preloader=({
-  children,
-}: {
-  children: React.ReactNode;
-})=> {
-  const [loading, setLoading] = useState(true);
+// export const Preloader=({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// })=> {
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setLoading(false);
+//     }, 3000);
 
-    return () => clearTimeout(timer);
-  }, []);
+//     return () => clearTimeout(timer);
+//   }, []);
 
-  if (loading) {
-    return (
-      <div style={preloaderStyles}>
-        <l-grid size="100" speed="1.5" color="white"></l-grid>
-      </div>
-    );
-  }
+//   if (loading) {
+//     return (
+//       <div style={preloaderStyles}>
+//         <l-grid size="100" speed="1.5" color="white"></l-grid>
+//       </div>
+//     );
+//   }
 
-  return <>{children}</>;
-}
+//   return <>{children}</>;
+// }
 
-const preloaderStyles: React.CSSProperties = {
-  position: "fixed",
-  top: 0,
-  left: 0,
-  width: "100%",
-  height: "100%",
-  backgroundColor: "black",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  zIndex: 9999,
-};
+// const preloaderStyles: React.CSSProperties = {
+//   position: "fixed",
+//   top: 0,
+//   left: 0,
+//   width: "100%",
+//   height: "100%",
+//   backgroundColor: "black",
+//   display: "flex",
+//   justifyContent: "center",
+//   alignItems: "center",
+//   zIndex: 9999,
+// };
